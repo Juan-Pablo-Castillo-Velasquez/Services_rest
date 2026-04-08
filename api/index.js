f57c05a8375bcac961a9bbe356b83d32ba93688f
Juan-Pablo-Castillo-Velasquez <juanpablo207k@gmail.com>
2026-04-07 22:53:50 -0500

    first commit

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../src/views"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const reservaController = require("../server/controllers/reservaController");
+
+app.get("/", (req, res) => {
+  res.render("Home_view", { titulo: "Bienvenido a la Home" });
+});
+
+app.get("/reservas", reservaController.listarReservas);
+
+app.post("/reservas/update", reservaController.updateReserva);
+
+module.exports = app;
